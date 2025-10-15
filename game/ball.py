1a79f094e47ef5e897922a7d79e90cc4ee1baea1
Gunika-Goel <gunika.goel.g@gmail.com>
2025-10-15 23:12:38 +0530

    Updated Task 4

--- game/ball.py
+++ game/ball.py
@@ -1,6 +1,8 @@
 import pygame
 import random
 
+pygame.mixer.init()
+
 class Ball:
     def __init__(self, x, y, width, height, screen_width, screen_height):
         self.original_x = x
@@ -13,6 +15,10 @@ class Ball:
         self.screen_height = screen_height
         self.velocity_x = random.choice([-5, 5])
         self.velocity_y = random.choice([-3, 3])
+        self.paddle_hit_sound = pygame.mixer.Sound("assets/sounds/ping_pong_hit.wav")
+        self.wall_bounce_sound = pygame.mixer.Sound("assets/sounds/ping_pong_bounce.wav")
+        self.score_sound = pygame.mixer.Sound("assets/sounds/score.wav")
+
 
     def move(self):
         self.x += self.velocity_x
@@ -20,6 +26,7 @@ class Ball:
 
         if self.y <= 0 or self.y + self.height >= self.screen_height:
             self.velocity_y *= -1
+            self.wall_bounce_sound.play()
 
     def check_collision(self, player, ai):
         ball_rect = self.rect()
@@ -35,6 +42,16 @@ class Ball:
         elif ball_rect.colliderect(ai_rect) and self.velocity_x > 0:
             self.velocity_x *= -1
             self.x = ai_rect.x - self.width
+        
+        if self.rect().colliderect(player.rect()) and self.velocity_x < 0:
+            self.velocity_x *= -1
+            self.x = player.rect().x + player.rect().width
+            self.paddle_hit_sound.play()  
+
+        elif self.rect().colliderect(ai.rect()) and self.velocity_x > 0:
+            self.velocity_x *= -1
+            self.x = ai.rect().x - self.width
+            self.paddle_hit_sound.play() 
 
 
   
@@ -44,6 +61,7 @@ class Ball:
         self.y = self.original_y
         self.velocity_x *= -1
         self.velocity_y = random.choice([-3, 3])
+        self.score_sound.play()
 
     def rect(self):
         return pygame.Rect(self.x, self.y, self.width, self.height)
